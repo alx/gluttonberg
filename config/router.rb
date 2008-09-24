afd16ee436652484b9158656257a944edddae9a4
Luke Sutton <lukesutton@ironcouncil.local>
2008-09-24 09:57:51 +0930

    Update the router config to work in Merb edge.

--- config/router.rb
+++ config/router.rb
@@ -1,30 +1,28 @@
 Merb.logger.info("Compiling routes...")
 Merb::Router.prepare do |r|
   # Administration paths
-  r.match("/admin").to(:namespace => "admin") do |a|
-    a.match("/content") do |c|
-      c.resources(:dialects)
-      c.resources(:locales)
-      c.resources(:templates) do |t|
-        t.resources(:sections, :controller => "template_sections")
+  match("/admin").to(:namespace => "admin") do
+    match("/content") do
+      resources(:dialects)
+      resources(:locales)
+      resources(:templates) do
+        resources(:sections, :controller => "template_sections")
       end
-      c.resources(:pages) do |p|
-        p.resources(:localizations, :controller => "page_localizations")
-        p.resources(:articles)
+      resources(:pages) do
+        resources(:localizations, :controller => "page_localizations")
+        resources(:articles)
       end
     end
     
-    a.match("/content").to(:controller => "content").name(:admin_content)
+    match("/content").to(:controller => "content").name(:content)
+    resources(:assets)
+    resources(:users)
     
-    a.resources(:assets)
-    
-    a.resources(:users)
-    
-    a.match("/settings").to(:controller => "settings") do |s|
-      s.match(:method => "get").to(:action => "index").name(:admin_settings)
-      s.match(:method => "put").to(:action => "update")
+    match("/settings").to(:controller => "settings") do |s|
+      match(:method => "get").to(:action => "index").name(:settings)
+      match(:method => "put").to(:action => "update")
     end
   end
   
-  r.match(/(.*)/).to(:controller => "public/pages", :action => "show", :path => "[1]")
+  match(/(.*)/).to(:controller => "public/pages", :action => "show", :path => "[1]")
 end
