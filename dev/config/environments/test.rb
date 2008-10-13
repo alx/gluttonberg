811f8b6749eb5351e5e7a456acac2e4221e3a67c
Luke Sutton <lukesutton@ironcouncil.local>
2008-10-13 14:18:38 +1030

    Add in a dev folder for bootstrapping a the slice in stand-alone. This is temporary and for development only.

---
+++ dev/config/environments/test.rb
@@ -0,0 +1,9 @@
+Merb.logger.info("Loaded TEST Environment...")
+Merb::Config.use { |c|
+  c[:testing] = true
+  c[:exception_details] = true
+  c[:log_auto_flush ] = true
+  c[:log_level] = :fatal
+}
+
+dependency 'dm-sweatshop'
