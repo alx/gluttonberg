3a9309e18bf66db0dc187fd827d06045d3402d4e
Luke Sutton <luke@freerangefuture.com>
2008-11-18 17:11:29 +1030

    Store the most useful params in a corresponding accessor, including the template names.

--- gluttonberg/app/controllers/content/public.rb
+++ gluttonberg/app/controllers/content/public.rb
@@ -3,13 +3,8 @@ module Gluttonberg
     class Public < Gluttonberg::Application
       include Gluttonberg::PublicController
       
-      #before :set_localization_and_path, :only => :show
-      
       def show
-        @pages = Page.all_with_localization(:parent_id => nil, :dialect => params[:dialect], :locale => params[:locale])
-        @page = params[:page]
-        templates = @page.template_paths(:dialect => params[:dialect], :locale => params[:locale])
-        render(:template => "pages/" + templates[:page], :layout => "#{templates[:layout]}.#{content_type}")
+        render(:template => page_template, :layout => page_layout)
       end
     end
   end
--- gluttonberg/lib/gluttonberg/public_controller.rb
+++ gluttonberg/lib/gluttonberg/public_controller.rb
@@ -2,49 +2,27 @@ module Gluttonberg
   module PublicController
     def self.included(klass)
       klass.class_eval do
-        attr_accessor :dialect, :locale, :path
+        attr_accessor :page, :dialect, :locale, :path, :page_template, :page_layout
         self._template_roots << [Gluttonberg::Templates.root, :_template_location]
+        before :store_models_and_templates
+        before :find_pages
       end
     end
     
     private
-
-    def set_localization_and_path
-      if Gluttonberg.localized? || Gluttonberg.translated?
-        # Get the dialect
-        # TODO: Have this only grab the active dialects
-        # TODO: fall back to the accept header i.e. request.env["HTTP_ACCEPT_LANGUAGE"]
-        @dialect = cascade_to_dialect(Dialect.all, params[:dialect])
-        # See if we need the locale as well
-        if Gluttonberg.localized?
-          @locale = Locale.first(:slug => params[:locale]) || Locale.first(:default => true)
-        end
-      end
-      
-      @path = params[:full_path]
+    
+    def find_pages
+      @pages = Page.all_with_localization(:parent_id => nil, :dialect => params[:dialect], :locale => params[:locale])
     end
-
-    def cascade_to_dialect(dialects, requested_dialect)
-      # If the dialects are empty, just return the default straight away.
-      #
-      # If we have dialects in our DB, let's try to find a match. If we don't
-      # have any matches, lets reduce the request lang and recurse until we find 
-      # a match or need to return the default.
-      if dialects.nil?
-        dialects.first(:default => true)
-      else
-        match = dialects.pluck {|d| d.code == requested_dialect}
-        if match
-          match
-        else
-          index = requested_dialect.rindex("-")
-          if index
-            cascade_to_dialect(dialects, requested_dialect[0, index])
-          else
-            dialects.first(:default => true)
-          end
-        end
-      end
+    
+    def store_models_and_templates
+      @dialect  = params[:dialect]
+      @locale   = params[:locale]
+      @page     = params[:page]
+      # Store the templates
+      templates       = @page.template_paths(:dialect => params[:dialect], :locale => params[:locale])
+      @page_template  = "pages/" + templates[:page]
+      @page_layout    = "#{templates[:layout]}.#{content_type}"
     end
   end
 end
